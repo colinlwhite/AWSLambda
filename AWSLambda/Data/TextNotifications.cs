8c9ac2d4039ebdf6b9799ec3be1cdbc3793dce37
Colin White <clwhite814@gmail.com>
2020-08-24 13:09:22 -0500

    Receiving text message when POST method is invoked

---
+++ AWSLambda/Data/TextNotifications.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace AWSLambda.Data
+{
+    public class TextNotifications
+    {
+        public void SendTextNotification()
+        {
+            var accountSid = "ACf52182c66da8da1a8e1d6dee3ea528c7";
+            var authToken = "4b8a41cd48a09e6b598a0da652ba5335";
+            var number = "+12052278229"; 
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "hello Colin",
+                from: new Twilio.Types.PhoneNumber("+16154929390"),
+                to: new Twilio.Types.PhoneNumber(number)
+                );
+        }
+    }
+}
