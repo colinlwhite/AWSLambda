7d0995b3e095f5981c513e14fae55500bfbf4c43
Colin White <clwhite814@gmail.com>
2020-08-27 12:34:58 -0500

    WIP: Twilio is not on most up-to-date branch

---
+++ AWSLambda/Data/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSLambda.Data
+{
+    public static class Globals
+    {
+
+        public static string twilioSID = "ACf52182c66da8da1a8e1d6dee3ea528c7";
+        public static string twilioAuthToken = "7f536db81a30a1d67012df743cb2e00f";
+        public static string inboundPhone = "+12052278229";
+        public static string outboundPhone = "+16154929390"; 
+
+
+    }
+}
