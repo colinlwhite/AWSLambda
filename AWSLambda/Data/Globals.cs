f48cc955fdc84fefea4bdf8966902cbf8af104c3
Colin White <clwhite814@gmail.com>
2020-08-28 11:24:56 -0500

    WIP

--- AWSLambda/Data/Globals.cs
+++ AWSLambda/Data/Globals.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace AWSLambda.Data
@@ -11,7 +12,8 @@ namespace AWSLambda.Data
         public static string twilioSID = "ACf52182c66da8da1a8e1d6dee3ea528c7";
         public static string twilioAuthToken = "7f536db81a30a1d67012df743cb2e00f";
         public static string inboundPhone = "+12052278229";
-        public static string outboundPhone = "+16154929390"; 
+        public static string outboundPhone = "+16154929390";
+        public static string testVariable = "somethinfdsfsdfsg";
 
 
     }
